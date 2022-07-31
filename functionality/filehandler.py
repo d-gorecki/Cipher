b8dcc4af96365153bf2cb4b2a2228cb0a52972be
d-gorecki <ja.dawidgorecki@gmail.com>
2022-07-31 19:34:43 +0200

    refactor: case handling method refactored

--- functionality/filehandler.py
+++ functionality/filehandler.py
@@ -9,14 +9,14 @@ class FileHandler:
         """Get file name(path) from user"""
         self.file_path: str = input("Please enter file name(path): ")
 
-    def read_file(self) -> str:
+    def read(self) -> str:
         """Read data from file and return it in form of str"""
         with open(self.file_path, mode="r") as f:
             output: str = "".join(f.readlines())
 
         return output
 
-    def write_file(self, text: str) -> None:
+    def write(self, text: str) -> None:
         """Write passed data to file. Append data in case file does exist.
         Create new file in case passed file does not exist."""
         with open(self.file_path, mode="a") as f:
--- functionality/ioreader.py
+++ functionality/ioreader.py
@@ -1,12 +1,12 @@
 class IOReader:
     """IOReader class"""
 
-    def io_read(self) -> str:
+    def read(self) -> str:
         """Read data from io stream and return it in form of str"""
         output: str = input("Please enter text:\n>>> ")
 
         return output
 
-    def io_write(self, text: str) -> None:
+    def write(self, text: str) -> None:
         """Print passed data to CLI"""
         print(text)
