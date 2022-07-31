7fdf5a334e79c5e83a525e1e91815d412a938362
d-gorecki <ja.dawidgorecki@gmail.com>
2022-07-31 18:37:44 +0200

    feat menu for output added, case keyboard-file added

--- functionality/facade.py
+++ functionality/facade.py
@@ -1,3 +1,4 @@
+from functionality.filehandler import FileHandler
 from functionality.cipher import ROT13
 
 
@@ -22,9 +23,23 @@ class Manager:
         ">>> "
     )
 
+    output_menu: str = (
+        "Please select output target:\n"
+        "1. Screen\n"
+        "2. File\n"
+        "3. Return to main menu\n"
+        ">>> "
+    )
+
     def __init__(self):
         self.cipher = {"ROT13": ROT13, "ROT47": "ROT47"}
-        self.input_ = {"keyboard": "IOReader", "file": "FileHandler"}
+        self.input = {"keyboard": "IOReader", "file": FileHandler}
+
+    def cipher_factory(self, cipher_key: str) -> ROT13:
+        return self.cipher.get(cipher_key)()
+
+    def input_factory(self, input_key: str) -> FileHandler:
+        return self.input.get(input_key)()
 
     def user_request_handler(self):
         """Perform action depending on user input"""
@@ -32,22 +47,33 @@ class Manager:
         main_menu_choice: int = int(input(Manager.main_menu))
         if main_menu_choice in [1, 2]:
             input_menu_choice = int(input(Manager.input_menu))
+            if input_menu_choice in [1, 2]:
+                output_menu_choice = int(input(Manager.output_menu))
         else:
             print("Closing app...")
             return
 
-        match (main_menu_choice, input_menu_choice):
-            case (1, 1):
-                print("ROT13 -> Keyboard")
-            case (1, 2):
-                print("ROT13 -> File")
-            case (1, 3):
-                print("Returning")
-            case (2, 1):
-                print("ROT47 -> Keyboard")
-            case (2, 2):
-                print("ROT47 -> File")
-            case (2, 3):
-                print("Returning")
+        match (main_menu_choice, input_menu_choice, output_menu_choice):
+            case (1, 1, 1):
+                print("ROT13 -> Keyboard -> Screen")
+
+            case (1, 2, 1):
+                print("ROT13 -> Keyboard -> File")
+                case_cipher = self.cipher_factory("ROT13")
+                case_output = self.input_factory("file")
+                user_input = input("Enter text to encode>>> ")
+                case_output.get_file_path()
+                case_output.write_file(case_cipher.encode(user_input))
+
+            case (1, 2, 1):
+                print("Rot13 -> File -> Screen")
+
+            case (1, 2, 2):
+                print("Rot13 -> File -> File")
+
             case _:
                 print("Something went wrong...")
+
+
+ex = Manager()
+ex.user_request_handler()
