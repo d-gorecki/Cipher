9fab4f4654b7b13458ee4be62c1b92c44c66570c
d-gorecki <ja.dawidgorecki@gmail.com>
2022-08-07 13:25:50 +0200

    refactor: cipher ABC and derived rot13 method refactored

--- functionality/cipher.py
+++ functionality/cipher.py
@@ -1,28 +1,29 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 import string
 
 
 class Cipher(ABC):
     """Abstract base class for ROT13 and ROT47 subclasses"""
 
-    def encode(self, input_text: str):
+    LOWERCASE: str = string.ascii_lowercase
+    UPPERCASE: str = string.ascii_uppercase
+
+    @abstractmethod
+    def encode_decode(self, input_text: str):
         raise NotImplementedError
 
 
 class ROT13(Cipher):
     """ROT13 class implementing ROT13 algorithm."""
 
-    lowercase: str = string.ascii_lowercase
-    uppercase: str = string.ascii_uppercase
-
-    def encode(self, input_text: str) -> str:
+    def encode_decode(self, input_text: str) -> str:
         """Return encoded/decoded text"""
         output_text: str = ""
         for char_ in input_text:
-            if char_ in ROT13.lowercase:
-                output_text += ROT13.lowercase[(ROT13.lowercase.find(char_) + 13) % 26]
-            elif char_ in ROT13.uppercase:
-                output_text += ROT13.uppercase[(ROT13.uppercase.find(char_) + 13) % 26]
+            if char_ in ROT13.LOWERCASE:
+                output_text += ROT13.LOWERCASE[(ROT13.LOWERCASE.find(char_) + 13) % 26]
+            elif char_ in ROT13.UPPERCASE:
+                output_text += ROT13.UPPERCASE[(ROT13.UPPERCASE.find(char_) + 13) % 26]
             else:
                 raise ValueError(
                     "ROT13 can encode only ASCII lower and uppercase letters."
