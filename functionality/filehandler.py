53f7edaa6978a82f20becb1a09bf5096739bb3ec
d-gorecki <ja.dawidgorecki@gmail.com>
2022-08-14 13:37:24 +0200

    feat: /files dir is now default dir for files output

--- functionality/filehandler.py
+++ functionality/filehandler.py
@@ -1,17 +1,20 @@
+from os import getcwd, mkdir, path
+
+
 class FileHandler:
     """File handler class"""
 
     def __init__(self):
-        self.file_path = ""
-        self.get_file_path()
+        self.file_name: str = ""
+        self.get_file_name()
 
-    def get_file_path(self) -> None:
+    def get_file_name(self) -> None:
         """Get file name(path) from user"""
-        self.file_path: str = input("Please enter file name(path): ")
+        self.file_name: str = input("Please enter file name: ")
 
     def read(self) -> str:
         """Read data from file and return it in form of str"""
-        with open(self.file_path, mode="r") as f:
+        with open(self.file_name, mode="r") as f:
             output: str = "".join(f.readlines())
 
         return output
@@ -19,5 +22,8 @@ class FileHandler:
     def write(self, text: str, cipher_type: str) -> None:
         """Write passed data to file. Append data in case file does exist.
         Create new file in case passed file does not exist."""
-        with open(self.file_path, mode="a") as f:
-            f.writelines(str({cipher_type: text}))
+        file_path: str = getcwd() + "/files"
+        if not path.isdir(file_path):
+            mkdir(file_path)
+        with open(file_path + f"/{self.file_name}", mode="a") as f:
+            f.writelines(str({cipher_type: text}) + "\n")
